c73bc41b738569b3879f5b0ae785858c37a43cf9
AyushRai <raiayush572@gmail.com>
2024-01-20 11:58:44 +0530

    Testinng Components

---
+++ CodeDefender/src/components/Footer.jsx
@@ -0,0 +1,10 @@
+
+import React from 'react';
+function Footer(){
+    return(
+        <div className="bg-blue-500">
+            <h1>Home</h1>
+        </div>
+    );
+}
+export default Footer;
--- CodeDefender/src/components/Home.jsx
+++ CodeDefender/src/components/Home.jsx
@@ -1,1 +1,10 @@
 
+import React from 'react';
+function Home(){
+    return(
+        <div className="bg-blue-500">
+            <h1>Home</h1>
+        </div>
+    );
+}
+export default Home;

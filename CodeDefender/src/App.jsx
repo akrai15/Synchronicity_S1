1c536d581c53d76b88f419497bf9f391f68ba453
AyushRai <raiayush572@gmail.com>
2024-01-20 11:46:45 +0530

    setting routes and adding assets

--- CodeDefender/src/App.jsx
+++ CodeDefender/src/App.jsx
@@ -1,11 +1,21 @@
 
-import './App.css'
+
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './components/Home';
+import Game from './components/Game';
+import About from './components/About';
+import Future from './components/Future';
 
 function App() {
- return (
-
-  <div className="bg-blue-500">hello</div>
-  )
+  return (
+    <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/game" element={<Game />} />
+        <Route path='/about' element={<About />} />
+        <Route path='/future' element={<Future />} />
+      </Routes>
+  );
 }
 
-export default App
+export default App;
